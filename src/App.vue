7e8e75411df9265578bad9fb924b6d07ca8663de
daddychain@proton.me <daddychain@proton.me>
2022-06-09 21:36:02 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -16,22 +16,5 @@ export default {
 }
 </script>
 <style lang="scss">
-.el-message {
-  padding: 8px 16px;
-  min-width: 150px;
-  box-shadow: 0 2px 6px rgba(0,0,0,.2);
-  pointer-events: all;
-  border-radius: 4px;
-  background-color: #fff;
-  border-color: #fff;
-  .el-icon-warning {
-    color: #f90;
-  }
-  .el-icon-success {
-    color: #19be6b;
-  }
-  .el-message__content {
-    color: #515a6e;
-  }
-}
+
 </style>
